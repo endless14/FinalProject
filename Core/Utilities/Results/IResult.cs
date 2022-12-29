b3322737115aec7951575f9e2f90fab973be636d
Şeref Çakıralp <seref_456@outlook.com>
2022-12-29 21:20:11 +0300

    29:12:2022 Ders sonu

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{   //temel voidler icin başlangıc
+     public interface IResult
+    {
+        bool Success { get; }
+        string Message { get; }
+    }
+}
